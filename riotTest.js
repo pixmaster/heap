97cf397e6e68a2153a968ad98337517f53c73414
Egor Shumov <pixmaster.vin@gmail.com>
2016-10-11 12:26:20 +0300

    fixed

--- riotTest.js
+++ riotTest.js
@@ -1,6 +1,3 @@
-riot.tag2('riottest', '', '', '', function(opts) {
+riot.tag2('riottest', 'RiotJS load test: {time} ms', '', '', function(opts) {
   this.time = (new Date) - start;
-
-RiotJS load test: {time} ms
-
 });
